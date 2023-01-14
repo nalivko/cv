c84e9031ac4f4bd0be074cf02bec7a78b9958c24
Dmitriy <poiuy_86@mail.ru>
2023-01-14 13:50:13 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const images = document.querySelectorAll('.slides img')
+const sliderLine = document.querySelector('.slides')
+let count = 0
+let width;
+
+function init() {
+    width = document.querySelector('.slider').offsetWidth;
+    // sliderLine.style.width = width * images.length + 'px'
+    images.forEach(item => {
+        item.style.width = width + 'px'
+        item.style.height = 'auto'
+    })
+    nextSlide()
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    count--
+    if (count < 0) {
+        count = images.length - 1
+    }
+    nextSlide()
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+    count++
+    if (count >= images.length) {
+        count = 0
+    }
+    nextSlide()
+})
+
+function nextSlide() {
+    sliderLine.style.transform = 'translate(-'+count * width + 'px)'
+}
+
+window.addEventListener('resize', init)
+init()
+
+
+const menuBtn = document.querySelector('.menu-btn')
+const menuCloseBtn = document.querySelector('.mobile-menu__close-btn')
+const menu = document.querySelector('.mobile-menu')
+
+menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('hidden')
+})
+
+menuCloseBtn.addEventListener('click', function () {
+    menu.classList.toggle('hidden')
+})
+
+menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('menu-active')
+    menuBtn.classList.toggle('menu-active')
+})
+
+document.querySelectorAll('.mobile-menu__item').forEach((elem) => {
+    elem.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    })
+})
